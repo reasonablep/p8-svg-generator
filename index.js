5fa9db576db948177dc6b5f9e06c3a8ba521cab5
reasonablep <140934359+reasonablep@users.noreply.github.com>
2023-10-29 18:28:56 -0700

    Fixed shapes.js and index.js. Now generating triangle and circle properly

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
-const inquirer = require(inquirer);
-const fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs')
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
 
 function generateSvg () {
 
     inquirer.prompt ([
         {
             type: 'input',
-            message: 'Please enter three character',
+            message: 'Please enter three characters for the logo.',
             name: 'text'
         },
         
 
         {
             type: 'input',
-            message: 'Please enter a color keyword or hexadecimal color code',
-            name: 'color'
+            message: 'Please enter a color keyword or hexadecimal color code for the text.',
+            name: 'textColor'
         },
 
         {
             type: 'list',
-            message: 'Please select a shape',
-            name: 'shape'
+            message: 'Please select a shape.',
+            name: 'shape',
+            choices: ['triangle',
+            'circle',
+            'square']
 
+        },
+
+        {
+            type: 'input',
+            message: 'Please enter a color keyword or hexadecimal color code for the shape.',
+            name: 'shapeColor'
         }
 
     ])
+
+.then(answers => {
+
+let userText = answers.text;
+let userTextColor = answers.textColor;
+let userShape = answers.shape;
+let userShapeColor = answers.shapeColor;
+
+console.log(userShape);
+
+let svgContent = '';
+
+if (userShape === 'circle') {
+
+    const userCircle = new Circle (userText, userTextColor, userShape, userShapeColor);
+    svgContent = userCircle.generateSvg();
+
+} else if (userShape === 'triangle') {
+
+    const userTriangle = new Triangle (userText, userTextColor, userShape, userShapeColor);
+    svgContent = userTriangle.generateSvg();
+
+} else if (userShape === 'square') {
+
+    const userSquare = new Square (userText, userTextColor, userShape, userShapeColor);
+    svgContent = userSquare.generateSvg();
+
 }
+
+    fs.writeFile('./examples/example.svg', svgContent, 'utf-8',   (err) => {
+        err ? console.log(err) :
+        console.log('SVG generated!');
+
+    })
+});
+
+};
+
 
 generateSvg();
